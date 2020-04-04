55eb5964d95ee4b1836e160f0c9e5cc8aab9f8da
brianlown <62453448+brianlown@users.noreply.github.com>
2020-04-04 10:12:38 -0400

    version incremented

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ package_data = \
 
 setup_kwargs = {
     'name': 'dtegui',
-    'version': '0.1.0',
+    'version': '0.2.0',
     'description': '',
     'long_description': None,
     'author': 'Your Name',
